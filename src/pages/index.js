eabb8a653ba5c87bdae2f36248ffb5532d1b803c
Harpeng <gondarev75@gmail.com>
2024-05-07 19:45:49 +0300

    statistic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
-import './index.css';
-import { accordion } from '../utils/constants';
-import { textDown} from '../components/accordion';
+import "./index.css";
+import { accordion, img, popup, popupEmailClose } from "../utils/constants";
+import { textDown } from "../components/accordion";
+import { closePopup, openPopup } from "../components/popup";
 
-accordion.forEach(title => {
-    title.addEventListener('click', textDown)
+accordion.forEach((title) => {
+  title.addEventListener("click", textDown);
+});
+
+img.addEventListener("click", () => {
+  openPopup(popup);
+});
+
+popupEmailClose.addEventListener("click", () => {
+  closePopup(popup);
 });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
 const accordion = document.querySelectorAll('.about__button');
+const img = document.querySelector('.inform__img');
+const popup = document.querySelector(".popup");
+const popupEmailClose = document.querySelector(".popup__close");
 
-export {accordion};
+
+export {accordion, img, popup, popupEmailClose};
